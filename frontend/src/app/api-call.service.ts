f6fbda8e2c7a311617eefb2d26495ab8d462436d
Masa Hilal H Abushamleh <Masa.Abushamleh@ibm.com>
2021-01-24 18:24:01 +0400

    frontend updated

--- frontend/src/app/api-call.service.ts
+++ frontend/src/app/api-call.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
