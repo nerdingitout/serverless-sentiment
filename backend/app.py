ad338535b3f321c4febc341800fd363b5412ac8c
s4saif <saif.rehman@ibm.com>
2021-01-24 14:12:48 +0400

    pushed latest channges

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,23 @@
+#!flask/bin/python
 from textblob import TextBlob
 from ibmcloudant.cloudant_v1 import CloudantV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from ibmcloudant.cloudant_v1 import Document, CloudantV1
+from flask import Flask, jsonify, abort, request
+import uuid 
 authenticator = IAMAuthenticator('UK-0lDYQNUHnrEWvCnQtH_-hG2AwRhQ0BUkUGs1atROs')
 service = CloudantV1(authenticator=authenticator)
 service.set_service_url('https://cbed9894-316b-43db-8d39-94a253532c6c-bluemix.cloudantnosqldb.appdomain.cloud')
-text = '''
-i  hate you
-'''
-blob = TextBlob(text)
-print(blob.sentiment)
-products_doc = Document(
-  id="small-appliances:1000042",
-  type=blob.polarity)
+app = Flask(__name__)
+@app.route('/api/post_sentiment', methods=['POST'])
+def post_sentiment():
+    if not request.json or not 'text' in request.json:
+        abort(400)
+    blob = TextBlob(request.json['text'])
+    print(blob.sentiment)
+    products_doc = Document(id="sentiment:"+ uuid.uuid4().hex[:6].lower(),sentiment=blob.polarity, text=request.json['text'])
+    response = service.post_document(db='sample', document=products_doc).get_result()
+    return jsonify({'message': 'succesfully posted to cloudant', "text": request.json['text'], "sentiment": blob.polarity }), 201
 
-response = service.post_document(db='sample', document=products_doc).get_result()
-
-print(response)
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
