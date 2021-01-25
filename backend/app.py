510dca8669cf70ffb5eb61cf75a258ee2647936e
s4saif <saif.rehman@ibm.com>
2021-01-25 14:46:12 +0400

    pushed latest channges

--- backend/app.py
+++ backend/app.py
@@ -5,10 +5,13 @@ from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from ibmcloudant.cloudant_v1 import Document, CloudantV1
 from flask import Flask, jsonify, abort, request
 import uuid 
+from flask_cors import CORS
+
 authenticator = IAMAuthenticator('UK-0lDYQNUHnrEWvCnQtH_-hG2AwRhQ0BUkUGs1atROs')
 service = CloudantV1(authenticator=authenticator)
 service.set_service_url('https://cbed9894-316b-43db-8d39-94a253532c6c-bluemix.cloudantnosqldb.appdomain.cloud')
 app = Flask(__name__)
+CORS(app)
 @app.route('/api/post_sentiment', methods=['POST'])
 def post_sentiment():
     if not request.json or not 'text' in request.json:
